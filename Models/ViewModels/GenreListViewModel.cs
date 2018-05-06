15b9df2e6ceaf409365d48ff9739285adae6697c
Jóhann Ívar Björnsson <johannivar@gmail.com>
2018-05-06 11:57:25 +0000

    GenreListViewModel
    forgot to save after defining variables

--- Models/ViewModels/GenreListViewModel.cs
+++ Models/ViewModels/GenreListViewModel.cs
@@ -2,6 +2,7 @@ namespace web.Models.ViewModels
 {
     public class GenreListViewModel
     {
-        
+        public int Id { get; set; } // To link to Genre details
+        public string Name { get; set; }
     }
 }
