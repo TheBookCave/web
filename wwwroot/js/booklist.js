a261d606254e02baf5ad54638ca057e248d71a51
Thomas Ari Bech <thomasb17@ru.is>
2018-05-09 14:56:07 +0000

    Moved the scripts for the booklist into another file

---
+++ wwwroot/js/booklist.js
@@ -0,0 +1,12 @@
+/// Shows the user what the current value of the price range in book list filter
+$("#currentPriceRangeValue").text($("#priceRange").val());
+
+/// Shows the user the current value of the price range in filters
+$("#priceRange").change(function() {
+  $("#currentPriceRangeValue").text($("#priceRange").val());
+});
+
+/// Enables tooltips for the page
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,18 +2,3 @@
 
 /// Sets the current page in the navbar as active
 $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
-
-
-
-/// Shows the user what the current value of the price range in book list filter
-// This should probably be moved to antoher js file
-$("#currentPriceRangeValue").text($("#priceRange").val());
-
-$("#priceRange").change(function() {
-  $("#currentPriceRangeValue").text($("#priceRange").val());
-});
-
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
