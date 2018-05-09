500fbcf3baa8f6522069a31812a7cfb71ea3ef20
kristin14 <kristin14@ru.is>
2018-05-09 15:14:22 +0000

    Migration to add BookID to OrderItems

---
+++ Migrations/20180509151143_add_bookid_column_to_orderItems_table.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace web.Migrations
+{
+    public partial class add_bookid_column_to_orderItems_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "OrderItems",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "OrderItems");
+        }
+    }
+}
