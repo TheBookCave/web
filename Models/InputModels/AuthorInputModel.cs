da1015e51aae78c8533abf6971b9ea972f4148ce
kristin14 <kristin14@ru.is>
2018-05-10 12:35:23 +0000

    Error Handling added for Input Author, Name field is required

--- Models/InputModels/AuthorInputModel.cs
+++ Models/InputModels/AuthorInputModel.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using web.Data.EntityModels;
 
 namespace web.Models.InputModels
 {
     public class AuthorInputModel
     {
+        [Required]
         public string Name { get; set; }
     }
 }
