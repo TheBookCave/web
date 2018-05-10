625f503ff84f3670878454542f8612c776267912
kristin14 <kristin14@ru.is>
2018-05-10 12:37:28 +0000

    Name Required for GenreInput Model

--- Models/InputModels/GenreInputModel.cs
+++ Models/InputModels/GenreInputModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace web.Models.InputModels
 {
     public class GenreInputModel
     {
+        [Required]
         public string Name { get; set; }
     }
 }
