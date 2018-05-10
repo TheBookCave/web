e721d90778df5f4efaf847dd74aa3fd26be61e45
Arnar <addi1204@gmail.com>
2018-05-10 21:14:44 +0000

    Saving FirstName and LastName to db

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -113,7 +113,7 @@ namespace web.Controllers
         {
             if(!ModelState.IsValid) { return View(); }
             
-            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
             var result = await _userManager.CreateAsync(user, model.Password);
 
             if(! await _roleManager.RoleExistsAsync("Staff"))
