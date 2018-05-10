4bd0a9f0f6ec0821138f1b244a2d2e59d1822b01
kristin14 <kristin14@ru.is>
2018-05-10 12:41:11 +0000

    Name Field for PublisherInputModel Requried

--- Models/InputModels/PublisherInputModel.cs
+++ Models/InputModels/PublisherInputModel.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using web.Data.EntityModels;
 
 namespace web.Models.InputModels
 {
     public class PublisherInputModel
     {
+        [Required]
         public string Name { get; set; }
     }
 }
