0b44360aef67fcd294585a80fcde2eb26593ef6f
Thomas Ari Bech <thomasb17@ru.is>
2018-05-11 23:50:13 +0000

    Format OrderSerivce.cs and OrderController.cs

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -15,190 +15,198 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace web.Controllers
 {
-    public class OrderController : Controller
+  public class OrderController : Controller
+  {
+    // OrderController owns an instance of the OrderService
+    private OrderService _orderService;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+
+    // Constructor for the BookController where the _bookService is created
+    public OrderController(DataContext context, UserManager<ApplicationUser> userManager)
     {
-        // OrderController owns an instance of the OrderService
-        private OrderService _orderService;
-        private readonly UserManager<ApplicationUser> _userManager;
+      _orderService = new OrderService(context);
+      _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+      var userId = _userManager.GetUserId(HttpContext.User);
+      var orderitems = _orderService.GetAllItemsInCart(userId);
+      return View(orderitems);
+    }
 
 
-        // Constructor for the BookController where the _bookService is created
-        public OrderController(DataContext context, UserManager<ApplicationUser> userManager)
-        {
-            _orderService = new OrderService(context);
-            _userManager = userManager;
-        }
-
-        public IActionResult Index() {
-          var userId = _userManager.GetUserId(HttpContext.User);
-          var orderitems = _orderService.GetAllItemsInCart(userId);
-          return View(orderitems);
-        }
+    [Authorize]
+    public IActionResult AddToCart(int BId)
+    {
+      var userId = _userManager.GetUserId(HttpContext.User);
 
 
-        [Authorize]
-        public IActionResult AddToCart(int BId)
-        {
-            var userId = _userManager.GetUserId(HttpContext.User);
-
-            
-            var book = _orderService.GetOrderItemInputModel(BId, userId);
-            if(ModelState.IsValid)
-            {
-                _orderService.AddToCart(book);
-                return RedirectToAction("Cart"); // ma breyta i ad redirecta einhvert annad
-            }
-            return View("Cart");
-        }
+      var book = _orderService.GetOrderItemInputModel(BId, userId);
+      if (ModelState.IsValid)
+      {
+        _orderService.AddToCart(book);
+        return RedirectToAction("Cart"); // ma breyta i ad redirecta einhvert annad
+      }
+      return View("Cart");
+    }
 
 
-        [Authorize]
-        public IActionResult Cart(string orderby, Order order)
-        {
-          var userId = _userManager.GetUserId(HttpContext.User);
-          var orderitems = _orderService.GetAllItemsInCart(userId);
-          return View(orderitems);
-        }
-        
+    [Authorize]
+    public IActionResult Cart(string orderby, Order order)
+    {
+      var userId = _userManager.GetUserId(HttpContext.User);
+      var orderitems = _orderService.GetAllItemsInCart(userId);
+      return View(orderitems);
+    }
 
 
-      [Authorize]
-      [HttpGet]
-      public IActionResult CheckOutReview() {
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult CheckOutReview()
+    {
       var userId = _userManager.GetUserId(HttpContext.User);
       var openorder = _orderService.GetOpenOrder(userId);
       var addressesUsed = _orderService.GetAddressesOnOrder(openorder.Id);
       var orderItems = _orderService.GetAllItemsInOrder(openorder.Id);
 
-      var ordertoconfirm = new OrderConfirmationViewModel() {
+      var ordertoconfirm = new OrderConfirmationViewModel()
+      {
         Order = openorder,
         Addresses = addressesUsed,
         OrderItems = orderItems
       };
 
       return View(ordertoconfirm);
-      }
-
-
-
-        [Authorize]
-
-        [HttpGet]
-        public IActionResult CheckOutAddress() {
-          var userId = _userManager.GetUserId(HttpContext.User);
-          var addressList = _orderService.GetUserAddresses(userId);
-          var openorder = _orderService.GetOpenOrder(userId);
-
-          var order = new OrderInputModel() {
-            AllUserAddresses = addressList
-          };
-
-          return View(order);
-        }
-
-
-        [Authorize]
-
-        [HttpPost]
-        public IActionResult CheckOutAddress(OrderInputModel order) { // 2 addresses, [0] is delivery, [1] is billing
-          var userId = _userManager.GetUserId(HttpContext.User);
-          var addressList = _orderService.GetUserAddresses(userId);
-          var openorder = _orderService.GetOpenOrder(userId);
-
-          order.Id = openorder.Id;
-          order.CustomerId = openorder.CustomerId;
-          order.Status = openorder.Status;
-          order.OrderDate = openorder.OrderDate;
-          order.ShippingDate = openorder.ShippingDate;
-          order.TrackingNumber = openorder.TrackingNumber;
-          order.PurchaseAmount = openorder.PurchaseAmount;
-          order.AllUserAddresses = addressList;
-
-            if(ModelState.IsValid)
-            {
-                _orderService.ChangeOrderAddress(order);
-                return RedirectToAction("CheckOutReview");
-
-            }
-          return View("CheckOutAddress", order);
-        }
-      
-
-
-      [Authorize]
-      [HttpPost]
-        public IActionResult CheckOutReview(OrderConfirmationViewModel ordertoconfirm) {
-          return View(ordertoconfirm);
-        }
-
-
-        [Authorize]
-        public IActionResult CheckOutConfirmed() {
-        var userId = _userManager.GetUserId(HttpContext.User);
-        _orderService.CloseOrder(userId);
-
-        return View();
-        }
-
-
-        public IActionResult Details(int Id)
-        {
-            var book = _orderService.GetOrderWithId(Id);
-
-            if (book == null)
-            {
-                return View("ErrorNotFound");
-            }
-            return View(book);
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-         [HttpPost]
-        public IActionResult Create(OrderInputModel inputOrder)
-        {
-            if(ModelState.IsValid)
-            {
-                _orderService.AddOrder(inputOrder);
-                return RedirectToAction("Index");
-            }
-            return View();
-        } 
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult AjaxRemoveOrderItem(int orderItemId)
-        {
-            if (ModelState.IsValid && orderItemId != 0)
-            {
-                _orderService.DeleteOrderItem(orderItemId);
-                return Ok();
-            }
-            return BadRequest();
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult AjaxChangeOrderItemQuantity(int orderItemId, bool increment)
-        {
-            if (ModelState.IsValid && orderItemId != 0)
-            {
-                if (increment) 
-                {
-                    _orderService.IncrementOrderItemQuantity(orderItemId);
-                }
-                else 
-                {
-                    _orderService.DecrementOrderItemQuantity(orderItemId);
-                }
-                return Ok();
-            }
-            return BadRequest();
-        }
     }
+
+
+
+    [Authorize]
+
+    [HttpGet]
+    public IActionResult CheckOutAddress()
+    {
+      var userId = _userManager.GetUserId(HttpContext.User);
+      var addressList = _orderService.GetUserAddresses(userId);
+      var openorder = _orderService.GetOpenOrder(userId);
+
+      var order = new OrderInputModel()
+      {
+        AllUserAddresses = addressList
+      };
+
+      return View(order);
+    }
+
+
+    [Authorize]
+
+    [HttpPost]
+    public IActionResult CheckOutAddress(OrderInputModel order)
+    { // 2 addresses, [0] is delivery, [1] is billing
+      var userId = _userManager.GetUserId(HttpContext.User);
+      var addressList = _orderService.GetUserAddresses(userId);
+      var openorder = _orderService.GetOpenOrder(userId);
+
+      order.Id = openorder.Id;
+      order.CustomerId = openorder.CustomerId;
+      order.Status = openorder.Status;
+      order.OrderDate = openorder.OrderDate;
+      order.ShippingDate = openorder.ShippingDate;
+      order.TrackingNumber = openorder.TrackingNumber;
+      order.PurchaseAmount = openorder.PurchaseAmount;
+      order.AllUserAddresses = addressList;
+
+      if (ModelState.IsValid)
+      {
+        _orderService.ChangeOrderAddress(order);
+        return RedirectToAction("CheckOutReview");
+
+      }
+      return View("CheckOutAddress", order);
+    }
+
+
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult CheckOutReview(OrderConfirmationViewModel ordertoconfirm)
+    {
+      return View(ordertoconfirm);
+    }
+
+
+    [Authorize]
+    public IActionResult CheckOutConfirmed()
+    {
+      var userId = _userManager.GetUserId(HttpContext.User);
+      _orderService.CloseOrder(userId);
+
+      return View();
+    }
+
+
+    public IActionResult Details(int Id)
+    {
+      var book = _orderService.GetOrderWithId(Id);
+
+      if (book == null)
+      {
+        return View("ErrorNotFound");
+      }
+      return View(book);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(OrderInputModel inputOrder)
+    {
+      if (ModelState.IsValid)
+      {
+        _orderService.AddOrder(inputOrder);
+        return RedirectToAction("Index");
+      }
+      return View();
+    }
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult AjaxRemoveOrderItem(int orderItemId)
+    {
+      if (ModelState.IsValid && orderItemId != 0)
+      {
+        _orderService.DeleteOrderItem(orderItemId);
+        return Ok();
+      }
+      return BadRequest();
+    }
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult AjaxChangeOrderItemQuantity(int orderItemId, bool increment)
+    {
+      if (ModelState.IsValid && orderItemId != 0)
+      {
+        if (increment)
+        {
+          _orderService.IncrementOrderItemQuantity(orderItemId);
+        }
+        else
+        {
+          _orderService.DecrementOrderItemQuantity(orderItemId);
+        }
+        return Ok();
+      }
+      return BadRequest();
+    }
+  }
 }
 
--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -10,151 +10,164 @@ using System.Security.Claims;
 
 namespace web.Services
 {
-    public class OrderService
+  public class OrderService
+  {
+    // OrderService owns a private instance of OrderRepo
+    private OrderRepo _orderRepo;
+
+    // Constructor for OrderService that creates the _orderRepo
+    public OrderService(DataContext context)
     {
-        // OrderService owns a private instance of OrderRepo
-        private OrderRepo _orderRepo;
+      _orderRepo = new OrderRepo(context);
+    }
 
-        // Constructor for OrderService that creates the _orderRepo
-        public OrderService(DataContext context)
-        {
-            _orderRepo = new OrderRepo(context);
-        }
+    // Function that return a list of all the orders
+    public List<OrderListViewModel> GetAllOrders()
+    {
+      var orders = _orderRepo.GetAllOrdersLinqQuery().ToList();
+      return orders;
+    }
 
-        // Function that return a list of all the orders
-        public List<OrderListViewModel> GetAllOrders()
-        {
-            var orders = _orderRepo.GetAllOrdersLinqQuery().ToList();
-            return orders;
-        }
+    public List<OrderItemListViewModel> GetAllOrderItems(int orderId)
+    {
+      var orderitems = _orderRepo.GetAllOrderItemsLinqQuery().ToList();
+      return orderitems;
+    }
 
-        public List<OrderItemListViewModel> GetAllOrderItems(int orderId)
-        {
-            var orderitems = _orderRepo.GetAllOrderItemsLinqQuery().ToList();
-            return orderitems;
-        }
+    public List<OrderItemListViewModel> GetAllItemsInCart(string userId)
+    {
+      var openOrderId = _orderRepo.GetOpenOrderId(userId);
+      //var openOrderId = 1;
+      var orderitemlist = _orderRepo.GetAllOrderItemsLinqQuery().Where(a => a.OrderId.Equals(openOrderId) && a.CustomerId == userId).ToList();
+      return orderitemlist;
+    }
 
-        public List <OrderItemListViewModel> GetAllItemsInCart (string userId) {
-            var openOrderId = _orderRepo.GetOpenOrderId(userId);
-            //var openOrderId = 1;
-            var orderitemlist = _orderRepo.GetAllOrderItemsLinqQuery().Where( a => a.OrderId.Equals(openOrderId) && a.CustomerId == userId).ToList();
-            return orderitemlist;
-        }
+    public List<OrderItemListViewModel> GetAllItemsInOrder(int orderId)
+    {
+      //var order = _orderRepo.GetOpenOrderId(userId);
+      //var openOrderId = 1;
+      var orderitemlist = _orderRepo.GetAllOrderItemsLinqQuery().Where(a => a.OrderId.Equals(orderId)).ToList();
+      return orderitemlist;
+    }
 
-        public List <OrderItemListViewModel> GetAllItemsInOrder (int orderId) {
-            //var order = _orderRepo.GetOpenOrderId(userId);
-            //var openOrderId = 1;
-            var orderitemlist = _orderRepo.GetAllOrderItemsLinqQuery().Where( a => a.OrderId.Equals(orderId)).ToList();
-            return orderitemlist;
-        }
+    public OrderItemInputModel GetOrderItemInputModel(int bookId, string userId)
+    {
 
-        public OrderItemInputModel GetOrderItemInputModel(int bookId, string userId) {
-
-            var oim = _orderRepo.GetOrderItemInputModel(bookId, userId);
-            return oim;
-
-        }
-
-        public List<AddressListViewModel> GetUserAddresses(string userId) {
-
-            return _orderRepo.GetUserAddresses(userId);
-        }
-
-        public OrderDetailViewModel GetOpenOrder(string userId) {
-            return _orderRepo.GetOpenOrder(userId);
-        }
-
-        public void OrderPurchaseSum(int orderId) {
-            _orderRepo.OrderPurchaseSum(orderId);
-        }
-
-
-        public void CloseOrder(string userId) {
-             _orderRepo.CloseOrder(userId);
-             return;
-        }
-
-        public void ChangeOrderAddress(OrderInputModel orderinput) {
-             _orderRepo.ChangeOrderAddress(orderinput);
-             return;
-        }
-
-        public List<AddressListViewModel> GetAddressesOnOrder(int orderId) {
-            return _orderRepo.GetAddressesOnOrder(orderId);
-        }
-
-        // Function that return a list of all the orders in alphabetic order
-        public List<OrderListViewModel> OrderByDate(List<OrderListViewModel> methodOrders)
-        {
-            var orders = methodOrders.OrderBy(x => x.OrderDate).ToList();
-            return orders;
-        }
-
-        // Function that return a list of all the orders in alphabetic order desc.
-        public List<OrderListViewModel> OrderByDateDesc(List<OrderListViewModel> methodOrders)
-        {
-            var orders = methodOrders.OrderByDescending(x => x.OrderDate).ToList();
-            return orders;
-        }
-
-        // Function that return a list of all the orders ordered by price
-        public List<OrderListViewModel> OrderByAmount(List<OrderListViewModel> methodOrders)
-        {
-            var orders = methodOrders.OrderBy(x => x.PurchaseAmount).ToList();
-            return orders;
-        }
-
-        // Function that return a list of all the orders ordered by price desc
-        public List<OrderListViewModel> OrderByAmountDesc(List<OrderListViewModel> methodOrders)
-        {
-            var orders = methodOrders.OrderByDescending(x => x.PurchaseAmount).ToList();
-            return orders;
-        }
-
-
-        // Function that return a order with specified ID
-        public OrderDetailViewModel GetOrderWithId(int Id)
-        {
-            var order = _orderRepo.GetOrderWithId(Id);
-            return order;
-        }
-
-        public void AddOrder(OrderInputModel inputOrder)
-        {
-            _orderRepo.AddOrder(inputOrder);
-        }
-
-        public void AddToCart(OrderItemInputModel orderinput) {
-
-            _orderRepo.AddToCart(orderinput);
-        }
-
-
-        public void IncrementOrderItemQuantity (int orderItemId) {
-            _orderRepo.IncrementOrderItemQuantity (orderItemId);
-        }
-
-        public void DecrementOrderItemQuantity (int orderItemId) {
-            _orderRepo.DecrementOrderItemQuantity (orderItemId);
-        }
-
-        public void DeleteOrderItem (int orderItemId) {
-            _orderRepo.DeleteOrderItem (orderItemId);
-        }
+      var oim = _orderRepo.GetOrderItemInputModel(bookId, userId);
+      return oim;
 
     }
-}
-        // Function that searches orders
-        /* 
-        public List<OrderListViewModel> SearchResults(string searchString)
-        {
-            if(searchString == null) 
-            {
-                searchString = "";
-            }
-            searchString = searchString.ToLower();
-            
-            var orders = _orderRepo.GetAllOrdersLinqQuery().Where( a => a.Author.ToLower().Contains(searchString) || a.Name.ToLower().Contains(searchString)).ToList();
 
-            return orders;
-        } */
+    public List<AddressListViewModel> GetUserAddresses(string userId)
+    {
+
+      return _orderRepo.GetUserAddresses(userId);
+    }
+
+    public OrderDetailViewModel GetOpenOrder(string userId)
+    {
+      return _orderRepo.GetOpenOrder(userId);
+    }
+
+    public void OrderPurchaseSum(int orderId)
+    {
+      _orderRepo.OrderPurchaseSum(orderId);
+    }
+
+
+    public void CloseOrder(string userId)
+    {
+      _orderRepo.CloseOrder(userId);
+      return;
+    }
+
+    public void ChangeOrderAddress(OrderInputModel orderinput)
+    {
+      _orderRepo.ChangeOrderAddress(orderinput);
+      return;
+    }
+
+    public List<AddressListViewModel> GetAddressesOnOrder(int orderId)
+    {
+      return _orderRepo.GetAddressesOnOrder(orderId);
+    }
+
+    // Function that return a list of all the orders in alphabetic order
+    public List<OrderListViewModel> OrderByDate(List<OrderListViewModel> methodOrders)
+    {
+      var orders = methodOrders.OrderBy(x => x.OrderDate).ToList();
+      return orders;
+    }
+
+    // Function that return a list of all the orders in alphabetic order desc.
+    public List<OrderListViewModel> OrderByDateDesc(List<OrderListViewModel> methodOrders)
+    {
+      var orders = methodOrders.OrderByDescending(x => x.OrderDate).ToList();
+      return orders;
+    }
+
+    // Function that return a list of all the orders ordered by price
+    public List<OrderListViewModel> OrderByAmount(List<OrderListViewModel> methodOrders)
+    {
+      var orders = methodOrders.OrderBy(x => x.PurchaseAmount).ToList();
+      return orders;
+    }
+
+    // Function that return a list of all the orders ordered by price desc
+    public List<OrderListViewModel> OrderByAmountDesc(List<OrderListViewModel> methodOrders)
+    {
+      var orders = methodOrders.OrderByDescending(x => x.PurchaseAmount).ToList();
+      return orders;
+    }
+
+
+    // Function that return a order with specified ID
+    public OrderDetailViewModel GetOrderWithId(int Id)
+    {
+      var order = _orderRepo.GetOrderWithId(Id);
+      return order;
+    }
+
+    public void AddOrder(OrderInputModel inputOrder)
+    {
+      _orderRepo.AddOrder(inputOrder);
+    }
+
+    public void AddToCart(OrderItemInputModel orderinput)
+    {
+
+      _orderRepo.AddToCart(orderinput);
+    }
+
+
+    public void IncrementOrderItemQuantity(int orderItemId)
+    {
+      _orderRepo.IncrementOrderItemQuantity(orderItemId);
+    }
+
+    public void DecrementOrderItemQuantity(int orderItemId)
+    {
+      _orderRepo.DecrementOrderItemQuantity(orderItemId);
+    }
+
+    public void DeleteOrderItem(int orderItemId)
+    {
+      _orderRepo.DeleteOrderItem(orderItemId);
+    }
+
+  }
+}
+// Function that searches orders
+/* 
+public List<OrderListViewModel> SearchResults(string searchString)
+{
+    if(searchString == null) 
+    {
+        searchString = "";
+    }
+    searchString = searchString.ToLower();
+
+    var orders = _orderRepo.GetAllOrdersLinqQuery().Where( a => a.Author.ToLower().Contains(searchString) || a.Name.ToLower().Contains(searchString)).ToList();
+
+    return orders;
+} */
