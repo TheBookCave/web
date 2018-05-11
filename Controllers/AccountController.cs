1659b931a8734cafe9c92dcc674fdf5e2ac97583
Thomas Ari Bech <thomasb17@ru.is>
2018-05-11 23:09:28 +0000

    Format Account and Book Controllers

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -22,190 +22,192 @@ using web.Repositories;
 
 namespace web.Controllers
 {
-    public class AccountController : Controller
+  public class AccountController : Controller
+  {
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    private DataContext _context;
+    private BookService _bookService;
+    private AuthenticationDbContext _aContext;
+    private OrderService _orderService;
+    private AccountService _accountService;
+
+    private readonly IHostingEnvironment _appEnvironment;
+    // var RoleManager = new RoleManager<IdentityRole>
+
+    public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IHostingEnvironment appEnvironment, DataContext context, AuthenticationDbContext aContext)
     {
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-
-        private DataContext _context;
-        private BookService _bookService;
-        private AuthenticationDbContext _aContext;
-        private OrderService _orderService;
-        private AccountService _accountService;
-        
-        private readonly IHostingEnvironment _appEnvironment;
-       // var RoleManager = new RoleManager<IdentityRole>
-
-        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IHostingEnvironment appEnvironment, DataContext context, AuthenticationDbContext aContext)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _roleManager = roleManager;
-            _appEnvironment = appEnvironment;
-            _aContext = aContext;
-            _context = context;
-            _bookService = new BookService(context, aContext);
-            _orderService = new OrderService(context);
-            _accountService = new AccountService(context, aContext);
-        }
-
-        [Authorize]
-        [HttpGet]
-        public IActionResult Index()
-        {
-            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var user = _userManager.Users.FirstOrDefault(x => x.Id == userId);
-            var _accountViewModel = _accountService.GetAccountViewModelByUser(user);
-            
-            return View(_accountViewModel);
-        }
-
-        [Authorize]
-        [HttpGet]
-        public IActionResult Edit()
-        {
-            
-            var _userEditInputModel = new UserEditInputModel();
-            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            _userEditInputModel.AllBooks = _bookService.GetAllBooks();
-            _userEditInputModel.AllAddresses = _orderService.GetUserAddresses(userId);
-            return View(_userEditInputModel);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(UserEditInputModel model)
-        {
-            if(!ModelState.IsValid) { 
-                return View(); 
-            }
-
-            //Get user
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-            await _accountService.UpdateUserChangedValues(user, model, _appEnvironment.WebRootPath);
-            
-            return RedirectToAction("Index", "Account");
-        }
-
-        
-        [Authorize]
-        [HttpGet]
-        public IActionResult CreateAddress()
-        {
-            return View();
-        }
-
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateAddress(AddressInputModel model)
-        {
-            if(!ModelState.IsValid) { 
-                return View(); 
-            }
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-            _accountService.AddAddressByUserId(model,user.Id);
-            return RedirectToAction("Index", "Account");
-        }
-
-
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterInputModel model)
-        {
-            if(!ModelState.IsValid) { return View(); }
-            
-            var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if(! await _roleManager.RoleExistsAsync("Staff"))
-            {
-                var staffRole = new IdentityRole();
-                staffRole.Name = "Staff";
-                await _roleManager.CreateAsync(staffRole);
-                await _userManager.AddToRoleAsync(user, "Staff");
-            }
-
-            if(result.Succeeded)
-            {
-                await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
-                await _signInManager.SignInAsync(user, false);
-
-                return RedirectToAction("Index", "Account");
-            }
-            return View();
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult RegisterStaff()
-        {
-            return View();
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> RegisterStaff(RegisterInputModel model)
-        {
-            if(!ModelState.IsValid) { return View(); }
-            
-            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if(result.Succeeded)
-            {
-                await _userManager.AddToRoleAsync(user, "Staff");
-                await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
-
-                return RedirectToAction("Index", "Staff");
-            }
-            return View();
-        }
-
-
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginInputModel model)
-        {
-            if(!ModelState.IsValid) { return View(); }
-            
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.Remember, false);
-
-            if(result.Succeeded)
-            {
-                return RedirectToAction("Index", "Account");
-            }
-            return View();
-        }
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Login", "Account");
-        }
-
-
-        public IActionResult AccessDenied()
-        {
-            return View();
-        }
-
+      _signInManager = signInManager;
+      _userManager = userManager;
+      _roleManager = roleManager;
+      _appEnvironment = appEnvironment;
+      _aContext = aContext;
+      _context = context;
+      _bookService = new BookService(context, aContext);
+      _orderService = new OrderService(context);
+      _accountService = new AccountService(context, aContext);
     }
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult Index()
+    {
+      var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+      var user = _userManager.Users.FirstOrDefault(x => x.Id == userId);
+      var accountViewModel = _accountService.GetAccountViewModelByUser(user);
+
+      return View(accountViewModel);
+    }
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult Edit()
+    {
+
+      var userEditInputModel = new UserEditInputModel();
+      var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+      userEditInputModel.AllBooks = _bookService.GetAllBooks();
+      userEditInputModel.AllAddresses = _orderService.GetUserAddresses(userId);
+      return View(userEditInputModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(UserEditInputModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View();
+      }
+
+      //Get user
+      var user = await _userManager.GetUserAsync(HttpContext.User);
+      await _accountService.UpdateUserChangedValues(user, model, _appEnvironment.WebRootPath);
+
+      return RedirectToAction("Index", "Account");
+    }
+
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult CreateAddress()
+    {
+      return View();
+    }
+
+    [Authorize]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateAddress(AddressInputModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View();
+      }
+      var user = await _userManager.GetUserAsync(HttpContext.User);
+      _accountService.AddAddressByUserId(model, user.Id);
+      return RedirectToAction("Index", "Account");
+    }
+
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterInputModel model)
+    {
+      if (!ModelState.IsValid) { return View(); }
+
+      var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
+      var result = await _userManager.CreateAsync(user, model.Password);
+
+      if (!await _roleManager.RoleExistsAsync("Staff"))
+      {
+        var staffRole = new IdentityRole();
+        staffRole.Name = "Staff";
+        await _roleManager.CreateAsync(staffRole);
+        await _userManager.AddToRoleAsync(user, "Staff");
+      }
+
+      if (result.Succeeded)
+      {
+        await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
+        await _signInManager.SignInAsync(user, false);
+
+        return RedirectToAction("Index", "Account");
+      }
+      return View();
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult RegisterStaff()
+    {
+      return View();
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> RegisterStaff(RegisterInputModel model)
+    {
+      if (!ModelState.IsValid) { return View(); }
+
+      var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+      var result = await _userManager.CreateAsync(user, model.Password);
+
+      if (result.Succeeded)
+      {
+        await _userManager.AddToRoleAsync(user, "Staff");
+        await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
+
+        return RedirectToAction("Index", "Staff");
+      }
+      return View();
+    }
+
+
+    public IActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginInputModel model)
+    {
+      if (!ModelState.IsValid) { return View(); }
+
+      var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.Remember, false);
+
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index", "Account");
+      }
+      return View();
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Login", "Account");
+    }
+
+
+    public IActionResult AccessDenied()
+    {
+      return View();
+    }
+
+  }
 }
--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -16,258 +16,258 @@ using Microsoft.AspNetCore.Identity;
 
 namespace web.Controllers
 {
-    public class BookController : Controller
+  public class BookController : Controller
+  {
+    // BookController owns an instance of the BookService
+    private BookService _bookService;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    // Constructor for the BookController where the _bookService is created
+    public BookController(DataContext context, AuthenticationDbContext aContext, UserManager<ApplicationUser> userManager)
     {
-        // BookController owns an instance of the BookService
-        private BookService _bookService;
-        private readonly UserManager<ApplicationUser> _userManager;
-
-        // Constructor for the BookController where the _bookService is created
-        public BookController(DataContext context, AuthenticationDbContext aContext, UserManager<ApplicationUser> userManager)
-        {
-            _bookService = new BookService(context, aContext);
-            _userManager = userManager;
-        }
-
-        public IActionResult Index(string orderby, string searchString2, string genreFilter)
-        {
-
-            var books = _bookService.GetAllBooks();
-
-            if (!String.IsNullOrEmpty(searchString2))
-            {
-                books = _bookService.SearchResults(searchString2);
-            }
-
-            if(!String.IsNullOrEmpty(genreFilter) && genreFilter != "0")
-            {
-                books = _bookService.FilterGenre(books, genreFilter);
-            }
-
-            if (orderby == "name-asc")
-            {
-                books = _bookService.OrderByName(books);
-            }
-            else if(orderby == "name-desc")
-            {
-                books = _bookService.OrderByNameDesc(books);
-            }
-            else if(orderby == "price-asc")
-            {
-                books = _bookService.OrderByPrice(books);
-            }
-            else if(orderby == "price-desc")
-            {
-                books = _bookService.OrderByPriceDesc(books);
-            }
-            else if(orderby == "rating-asc")
-            {
-                books = _bookService.OrderByRating(books);
-            }
-            else if(orderby == "rating-desc")
-            {
-                books = _bookService.OrderByRatingDesc(books);
-            }
-    
-            return View(books);
-        }
-
-        [HttpGet]
-        public IActionResult GetAllGenres()
-        {
-            var genres = _bookService.GetAllGenres();
-            return Json(genres);
-        }
-
-        public IActionResult Top10()
-        {
-            var books = _bookService.GetTop10Books();
-            
-            return View(books);
-        }
-
-
-        public IActionResult Details(int Id)
-        {
-            var book = _bookService.GetBookWithId(Id);
-
-            if (book == null)
-            {
-                return View("ErrorNotFound");
-            }
-            return View(book);
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult Create()
-        {
-            var book = _bookService.CreateNewBookInputModel(); // Create new BookInputViewModel and add Genres, Authors and Publishers
-            
-            return View(book);
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpPost]
-        public IActionResult Create(BookInputModel inputBook)
-        {
-            if(ModelState.IsValid)
-            {
-                _bookService.AddBook(inputBook);
-                return RedirectToAction("Index", "Staff");
-            }
-                // Add Genres, Authors and Publishers to the inputBook so that dropdownlists and checkboxes will be populated.
-                inputBook.AllGenres = _bookService.GetAllGenres();
-                inputBook.AllAuthors = _bookService.GetAllAuthors();
-                inputBook.AllPublishers = _bookService.GetAllPublishers();
-            
-            return View(inputBook);
-        } 
-
-        [Authorize]
-        [HttpGet]
-        public IActionResult RateBook(int BookId, string BookName)
-        {
-
-            var book = new RatingInputModel
-            {
-                BookId = BookId,
-                BookName = BookName,
-                CustomerId = "",
-                RatingValue = 0,
-                Comment = "",
-                RatingDate = DateTime.Now
-            };
-            
-            return View(book);
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult RateBook(RatingInputModel rating)
-        {
-            var userId = _userManager.GetUserId(HttpContext.User);
-            rating.CustomerId = userId; 
-            rating.RatingDate = DateTime.Now;
-
-
-            if(ModelState.IsValid)
-            {
-                _bookService.AddRating(rating);
-                return RedirectToAction("Details", new {Id = rating.BookId});
-            }
-            return View(rating);
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpPost]
-        public IActionResult CreateGenre(GenreInputModel inputGenre)
-        {
-            if(ModelState.IsValid)
-            {
-                _bookService.AddGenre(inputGenre);
-                return RedirectToAction("Index", "Staff");
-            }
-            return View();
-        } 
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult CreateGenre()
-        {
-            return View();
-        }
-
-
-        [Authorize(Roles = "Staff")]
-        [HttpPost]
-        public IActionResult CreateAuthor(AuthorInputModel inputAuthor)
-        {
-            if(ModelState.IsValid)
-            {
-                _bookService.AddAuthor(inputAuthor);
-                return RedirectToAction("Index", "Staff");
-            }
-            return View();
-        } 
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult CreateAuthor()
-        {
-            return View();
-        }
-
-        public IActionResult CreatePublisher(PublisherInputModel inputPublisher)
-        {
-            if(ModelState.IsValid)
-            {
-                _bookService.AddPublisher(inputPublisher);
-                return RedirectToAction("Index", "Staff");
-            }
-            return View();
-        } 
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult CreatePublisher()
-        {
-            return View();
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult ChooseBookToEdit()
-        {
-            var _editBookModel = new EditBookInputModel();
-            _editBookModel.AllBooks = _bookService.GetAllBooks();
-            
-            return View(_editBookModel);
-        }
-        
-        [Authorize(Roles = "Staff")]
-        [HttpPost]
-        public IActionResult ChooseBookToEdit(EditBookInputModel book)
-        {
-            return RedirectToAction("ModifyBook", new{bookId = book.BookId});
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpGet]
-        public IActionResult ModifyBook(int bookId)
-        {
-            var b2 = _bookService.GetEditBookWithId(bookId);
-
-            b2.AllAuthors = _bookService.GetAllAuthors();
-            b2.AllPublishers = _bookService.GetAllPublishers();
-            b2.AllGenres = _bookService.GetAllGenres();
-
-            return View(b2);
-        }
-
-        [Authorize(Roles = "Staff")]
-        [HttpPost]
-        public IActionResult ModifyBook(EditBookInputModel book)
-        {
-            if(ModelState.IsValid)
-            {
-                _bookService.ModifyBook(book);
-                return RedirectToAction("Index", "Staff");
-            }
-
-            book.AllAuthors = _bookService.GetAllAuthors();
-            book.AllPublishers = _bookService.GetAllPublishers();
-            book.AllGenres = _bookService.GetAllGenres();
-            return View(book); 
-        }
-
-        [HttpGet]
-        public IActionResult Search(string searchString)
-        {
-            var foundBooks = _bookService.SearchResults(searchString);
-            return View(foundBooks);
-        }
-
-
+      _bookService = new BookService(context, aContext);
+      _userManager = userManager;
     }
+
+    public IActionResult Index(string orderby, string searchString2, string genreFilter)
+    {
+
+      var books = _bookService.GetAllBooks();
+
+      if (!String.IsNullOrEmpty(searchString2))
+      {
+        books = _bookService.SearchResults(searchString2);
+      }
+
+      if (!String.IsNullOrEmpty(genreFilter) && genreFilter != "0")
+      {
+        books = _bookService.FilterGenre(books, genreFilter);
+      }
+
+      if (orderby == "name-asc")
+      {
+        books = _bookService.OrderByName(books);
+      }
+      else if (orderby == "name-desc")
+      {
+        books = _bookService.OrderByNameDesc(books);
+      }
+      else if (orderby == "price-asc")
+      {
+        books = _bookService.OrderByPrice(books);
+      }
+      else if (orderby == "price-desc")
+      {
+        books = _bookService.OrderByPriceDesc(books);
+      }
+      else if (orderby == "rating-asc")
+      {
+        books = _bookService.OrderByRating(books);
+      }
+      else if (orderby == "rating-desc")
+      {
+        books = _bookService.OrderByRatingDesc(books);
+      }
+
+      return View(books);
+    }
+
+    [HttpGet]
+    public IActionResult GetAllGenres()
+    {
+      var genres = _bookService.GetAllGenres();
+      return Json(genres);
+    }
+
+    public IActionResult Top10()
+    {
+      var books = _bookService.GetTop10Books();
+
+      return View(books);
+    }
+
+
+    public IActionResult Details(int Id)
+    {
+      var book = _bookService.GetBookWithId(Id);
+
+      if (book == null)
+      {
+        return View("ErrorNotFound");
+      }
+      return View(book);
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult Create()
+    {
+      var book = _bookService.CreateNewBookInputModel(); // Create new BookInputViewModel and add Genres, Authors and Publishers
+
+      return View(book);
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpPost]
+    public IActionResult Create(BookInputModel inputBook)
+    {
+      if (ModelState.IsValid)
+      {
+        _bookService.AddBook(inputBook);
+        return RedirectToAction("Index", "Staff");
+      }
+      // Add Genres, Authors and Publishers to the inputBook so that dropdownlists and checkboxes will be populated.
+      inputBook.AllGenres = _bookService.GetAllGenres();
+      inputBook.AllAuthors = _bookService.GetAllAuthors();
+      inputBook.AllPublishers = _bookService.GetAllPublishers();
+
+      return View(inputBook);
+    }
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult RateBook(int BookId, string BookName)
+    {
+
+      var book = new RatingInputModel
+      {
+        BookId = BookId,
+        BookName = BookName,
+        CustomerId = "",
+        RatingValue = 0,
+        Comment = "",
+        RatingDate = DateTime.Now
+      };
+
+      return View(book);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult RateBook(RatingInputModel rating)
+    {
+      var userId = _userManager.GetUserId(HttpContext.User);
+      rating.CustomerId = userId;
+      rating.RatingDate = DateTime.Now;
+
+
+      if (ModelState.IsValid)
+      {
+        _bookService.AddRating(rating);
+        return RedirectToAction("Details", new { Id = rating.BookId });
+      }
+      return View(rating);
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpPost]
+    public IActionResult CreateGenre(GenreInputModel inputGenre)
+    {
+      if (ModelState.IsValid)
+      {
+        _bookService.AddGenre(inputGenre);
+        return RedirectToAction("Index", "Staff");
+      }
+      return View();
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult CreateGenre()
+    {
+      return View();
+    }
+
+
+    [Authorize(Roles = "Staff")]
+    [HttpPost]
+    public IActionResult CreateAuthor(AuthorInputModel inputAuthor)
+    {
+      if (ModelState.IsValid)
+      {
+        _bookService.AddAuthor(inputAuthor);
+        return RedirectToAction("Index", "Staff");
+      }
+      return View();
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult CreateAuthor()
+    {
+      return View();
+    }
+
+    public IActionResult CreatePublisher(PublisherInputModel inputPublisher)
+    {
+      if (ModelState.IsValid)
+      {
+        _bookService.AddPublisher(inputPublisher);
+        return RedirectToAction("Index", "Staff");
+      }
+      return View();
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult CreatePublisher()
+    {
+      return View();
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult ChooseBookToEdit()
+    {
+      var editBookModel = new EditBookInputModel();
+      editBookModel.AllBooks = _bookService.GetAllBooks();
+
+      return View(editBookModel);
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpPost]
+    public IActionResult ChooseBookToEdit(EditBookInputModel book)
+    {
+      return RedirectToAction("ModifyBook", new { bookId = book.BookId });
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpGet]
+    public IActionResult ModifyBook(int bookId)
+    {
+      var b2 = _bookService.GetEditBookWithId(bookId);
+
+      b2.AllAuthors = _bookService.GetAllAuthors();
+      b2.AllPublishers = _bookService.GetAllPublishers();
+      b2.AllGenres = _bookService.GetAllGenres();
+
+      return View(b2);
+    }
+
+    [Authorize(Roles = "Staff")]
+    [HttpPost]
+    public IActionResult ModifyBook(EditBookInputModel book)
+    {
+      if (ModelState.IsValid)
+      {
+        _bookService.ModifyBook(book);
+        return RedirectToAction("Index", "Staff");
+      }
+
+      book.AllAuthors = _bookService.GetAllAuthors();
+      book.AllPublishers = _bookService.GetAllPublishers();
+      book.AllGenres = _bookService.GetAllGenres();
+      return View(book);
+    }
+
+    [HttpGet]
+    public IActionResult Search(string searchString)
+    {
+      var foundBooks = _bookService.SearchResults(searchString);
+      return View(foundBooks);
+    }
+
+
+  }
 }
