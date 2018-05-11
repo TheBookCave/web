1479b26eb16348be9083ac50557cf5e48da46e90
Thomas Ari Bech <thomasb17@ru.is>
2018-05-11 19:08:52 +0000

    Added a bit of validation and feedback for the register accounts

--- Models/InputModels/RegisterInputModel.cs
+++ Models/InputModels/RegisterInputModel.cs
@@ -15,6 +15,7 @@ namespace web.Models.InputModels
         public string LastName { get; set; }
 
         [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\\d]){1,})(?=(.*[\\W]){1,})(?!.*\\s).{8,}$", ErrorMessage = "Password must have a minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
         public string Password { get; set; }
     }
 }
